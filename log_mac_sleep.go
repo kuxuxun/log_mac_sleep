d875f11e6c4a653b8dc26b41fca07ab91c540474
kawada <kawada@nebloe.org>
2015-09-09 09:48:50 +0900

    impl aggregating logic

--- log_mac_sleep.go
+++ log_mac_sleep.go
@@ -2,10 +2,13 @@ package log_mac_sleep
 
 /* example: output comment to console on sleep/wake up */
 import (
+	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"os/user"
 	"path/filepath"
+	"strings"
 	"sync"
 	"time"
 
@@ -13,11 +16,29 @@ import (
 )
 
 var (
-	mutex       = &sync.Mutex{}
-	jst         = time.FixedZone("Asia/Tokyo", 9*60*60)
-	LogTimeFmt  = "2006-01-02 15:04:05"
-	LogFileName = ".sleeplog/log"
+	mutex                 = &sync.Mutex{}
+	jst                   = time.FixedZone("Asia/Tokyo", 9*60*60)
+	LogTimeFmt            = "2006-01-02 15:04:05"
+	outDateFmt            = "2006-01-02"
+	outTimeFmt            = "15:04:05"
+	LogFileName           = ".sleeplog/log"
+	AggrFileName          = ".sleeplog/log_aggre"
+	endToBeginThreshold   = 6 * time.Hour           // rolling a day if interval of end to begin over this term.
+	beginToBeginThreshold = 3 * endToBeginThreshold // rolling a day if interval of begin to begin over this term. (suggesting there were no "end" action between this term)
+	locationJP, _         = time.LoadLocation("Asia/Tokyo")
 )
+
+const (
+	IS_START int = iota
+	IS_END
+)
+
+var actionTypes = map[string]int{
+	"start":    IS_START,
+	"wakeup":   IS_START,
+	"sleep":    IS_END,
+	"poweroff": IS_END,
+}
 
 func Start() {
 	mac_switch_watch.SetHandler(mac_switch_watch.KeyOnSystemWillSleep, OnSleep)
@@ -50,7 +71,6 @@ func fmtJst(t time.Time, format string) string {
 }
 
 func logTimeToFile(msg string) {
-
 	usr, _ := user.Current()
 	homeDir := usr.HomeDir
 	logFilePath := filepath.Join(homeDir, LogFileName)
@@ -69,4 +89,109 @@ func logTimeToFile(msg string) {
 	file.WriteString(log)
 
 	defer file.Close()
+}
+
+func aggrLog(msg string) {
+	usr, _ := user.Current()
+	homeDir := usr.HomeDir
+	logFilePath := filepath.Join(homeDir, LogFileName)
+
+	if _, err := os.Stat(filepath.Dir(logFilePath)); os.IsNotExist(err) {
+		os.MkdirAll(filepath.Dir(logFilePath), 0744)
+	}
+
+	logFile, err := os.Open(logFilePath)
+	if err != nil {
+		panic(err)
+	}
+	defer logFile.Close()
+
+	logFileScanner := bufio.NewScanner(logFile)
+
+	workingTimeADays, err := aggregate(logFileScanner)
+	if err != nil {
+		panic(err)
+	}
+
+	aggrFilePath := filepath.Join(homeDir, AggrFileName)
+	aggrfile, _ := os.OpenFile(aggrFilePath, os.O_RDWR|os.O_CREATE, 0644)
+	for _, workTime := range workingTimeADays {
+		aggrfile.WriteString(workTime.ToCsvLine())
+	}
+}
+
+type WorkingTimeADay struct {
+	Start time.Time
+	End   time.Time
+}
+
+func (w WorkingTimeADay) ToCsvLine() string {
+	var dt string
+	if w.Start.IsZero() {
+		dt = fmtJst(w.End, outDateFmt)
+	} else {
+		dt = fmtJst(w.Start, outDateFmt)
+	}
+
+	s := fmtJst(w.Start, outTimeFmt)
+	e := fmtJst(w.End, outTimeFmt)
+
+	return fmt.Sprintf("%s,%s,%s", dt, s, e)
+}
+
+func aggregate(logFileScanner *bufio.Scanner) ([]WorkingTimeADay, error) {
+	var workingTimes []WorkingTimeADay
+	workingTime := WorkingTimeADay{}
+
+	for logFileScanner.Scan() {
+		cols := strings.Split(logFileScanner.Text(), ":")
+		if len(cols) != 2 {
+			panic(errors.New(fmt.Sprintf("invalid line %#v", cols)))
+		}
+		action, timeStr := cols[0], cols[1]
+		actionType, ok := actionTypes[action]
+		if !ok {
+			panic(errors.New(fmt.Sprintf("invalid action %s", action)))
+		}
+
+		logedTime, err := time.ParseInLocation(LogTimeFmt, timeStr, locationJP)
+		if err != nil {
+			return nil, err
+		}
+
+		switch actionType {
+		case IS_START:
+			if workingTime.End.IsZero() {
+				if logedTime.After(workingTime.Start.Add(beginToBeginThreshold)) {
+					workingTimes = append(workingTimes, workingTime)
+					workingTime = WorkingTimeADay{}
+					workingTime.Start = logedTime
+				}
+			} else if logedTime.After(workingTime.End.Add(endToBeginThreshold)) {
+				workingTimes = append(workingTimes, workingTime)
+				workingTime = WorkingTimeADay{}
+				workingTime.Start = logedTime
+			} else {
+				continue
+			}
+
+			if workingTime.Start.IsZero() {
+				workingTime.Start = logedTime
+				continue
+			}
+
+		case IS_END:
+			workingTime.End = logedTime
+
+		default:
+			panic("not impl")
+
+		}
+
+		workingTimes = append(workingTimes, workingTime)
+	}
+
+	workingTimes = append(workingTimes, workingTime)
+
+	return workingTimes, nil
 }
