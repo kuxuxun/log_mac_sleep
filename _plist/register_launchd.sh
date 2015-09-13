27e06ebc407508054f4e6ee48bfa5cb83c7b68e5
kawada <kawada@nebloe.org>
2015-09-13 15:00:58 +0900

    some

--- _plist/register_launchd.sh
+++ _plist/register_launchd.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 cp $GOPATH/src/github.com/kuxuxun/log_mac_sleep/_plist/github.com.kuxuxun.log_mac_sleep.plist  ~/Library/LaunchAgents/
-launchctl setenv PATH $GOPATH/bin:$PATH
 launchctl load ~/Library/LaunchAgents/github.com.kuxuxun.log_mac_sleep.plist
 launchctl start github.com.kuxuxun.log_mac_sleep.plist
